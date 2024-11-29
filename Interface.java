2fb8cf62d84b1090012fab5bf0c32236f6cf6e4e
Isaias Angell Fontes Soares <123341479+IsaiahAngell@users.noreply.github.com>
2024-11-29 11:37:52 -0300

    Interface.java
    Interface that's going to be used by the class Lutador

---
+++ Interface.java
@@ -0,0 +1,7 @@
+public interface Interface{
+  public abstract void ganharLuta();
+  public abstract void perderLuta();
+  public abstract void empatarLuta();
+  public abstract void apresentar();
+  public abstract void status();
+}
